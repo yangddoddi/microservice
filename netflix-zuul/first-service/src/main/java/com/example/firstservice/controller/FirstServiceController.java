f4cf9902558ddef8105a9a4795e35c1c1163690f
yangddoddi <pud1251@gmail.com>
2023-01-31 22:32:38 +0900

    gateway + naming server + 4 service server

--- netflix-zuul/first-service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
+++ netflix-zuul/first-service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
@@ -1,13 +1,23 @@
 package com.example.firstservice.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/first-service")
 public class FirstServiceController {
+    Environment environment;
+
     @GetMapping("/welcome")
     public String welcome(@RequestHeader(name = "first-request") String header) {
         System.out.println(header);
@@ -20,7 +30,8 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String message() {
-        return "hi, there. this is a message from first service";
+    public String message(HttpServletRequest httpServletRequest) {
+        log.info("{}",httpServletRequest.getServerPort());
+        return Mono.just(httpServletRequest.getServerPort()).toString();
     }
 }
