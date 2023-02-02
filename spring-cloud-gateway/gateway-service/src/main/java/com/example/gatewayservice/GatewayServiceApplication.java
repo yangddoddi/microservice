66483b63f580563791d4489fa5e0857dd059c545
yangddoddi <pud1251@gmail.com>
2023-02-02 15:42:49 +0900

    gateway + actuator

--- spring-cloud-gateway/gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ spring-cloud-gateway/gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -1,8 +1,11 @@
 package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -12,4 +15,8 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
 }
